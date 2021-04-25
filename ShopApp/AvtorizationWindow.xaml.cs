d0d3f9133a3b79a37dcd8b1c43e5debc36091a66
Dima <dmitry.turov374@gmail.com>
2021-04-25 17:35:34 +0300

    First commit

---
+++ ShopApp/AvtorizationWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ShopApp
+{
+    public partial class AvtorizationWindow : Window
+    {
+        public AvtorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Avtorization_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
